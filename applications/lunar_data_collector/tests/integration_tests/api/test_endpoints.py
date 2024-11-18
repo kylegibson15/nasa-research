ea341b64110b720de138ec200d6ae5214c172da0
kylegibson15 <kylegibson15@gmail.com>
2024-11-18 00:07:49 -0700

    does it have a heart

--- applications/lunar_data_collector/tests/integration_tests/api/test_endpoints.py
+++ applications/lunar_data_collector/tests/integration_tests/api/test_endpoints.py
@@ -11,7 +11,7 @@ class TestEndpoints:
         response: list[Mission] = client.get("/missions")
         data = response.json()
         assert response.status_code == 200
-        assert len(data) >= 0
+        # assert len(data) >= 0
         # first_mission: Mission = data[0]
         # assert first_mission["name"] == "Luna 20"
         # samples = first_mission["samples"]
